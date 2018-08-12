f65d1421c648a0e12708a70c9fde6a7a142a8895
Yogesh <yogesh.pradhan67@gmail.com>
2018-08-12 07:15:44 -0400

    Add feature to edit the blog post.

--- blog/urls.py
+++ blog/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
 
     #127.0.0.1:8000/post/new
     path('post/new/', views.post_new, name='post_new'),
+
+    #127.0.0.1:8000/1/edit
+    path('post/<int:pk>/edit', views.post_edit, name='post_edit')
 ]
