8b7a0613088ee78f6efd2f57d77072abb082088a
Yogesh <yogesh.pradhan67@gmail.com>
2018-08-12 06:12:11 -0400

    Add feature to create new blog posts from front end

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
 
     #127.0.0:8000/post/2
     path('post/<int:pk>', views.post_detail, name='post_detail'),
+
+    #127.0.0.1:8000/post/new
+    path('post/new/', views.post_new, name='post_new'),
 ]
